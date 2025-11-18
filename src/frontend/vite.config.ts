4526fd6ae28bb8de943ec836c6af8fd85c55c936
ASF1332 <eng.judre@gmail.com>
2025-11-17 23:09:22 -0300

    feat: Adiciona scripts de build e start para produção do backend

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174, // Define a porta padrão para 5174
+    strictPort: true, // Se a porta 5174 estiver ocupada, o Vite vai falhar em vez de tentar outra
+  }
 })
