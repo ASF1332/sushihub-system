ac80913534a74a50b21c185c286d44e7f5552a34
ASF1332 <eng.judre@gmail.com>
2025-12-30 19:36:32 -0300

    Fix: Correção da porta do backend e Debug visual

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const prisma = new PrismaClient();
-const PORT = process.env.PORT || 3001;
+const PORT = Number(process.env.PORT) || 3001;
 
 app.use(cors());
 app.use(express.json());
